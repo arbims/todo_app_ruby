f83cc91abea9baf1e8acbee496200fc4931e91ab
arbi <msahelarbi@gmail.com>
2022-06-04 16:25:43 +0200

    add+remove [todos|dones]

--- app.rb
+++ app.rb
@@ -19,12 +19,34 @@ class Ui
     win.addstr(label)
     win.attroff(color_pair(pair))
   end
+
   def label_not_win(row, col,label, pair)
     attron(color_pair(pair)| A_BLINK)
     setpos(row, col)
     addstr(label)
     attroff(color_pair(pair))
   end
+
+  def toggle(tab)
+		tab == 'TODO' ? 'DONE' : 'TODO'
+	end
+
+  def list_up(lists, list_curr)
+		if list_curr and list_curr > 0
+			list_curr = list_curr - 1
+		else
+			list_curr = 0
+		end 
+	end
+
+  def list_down(lists, list_curr)
+		if list_curr and list_curr < lists.length
+			list_curr = list_curr + 1 
+		else
+			list_curr = lists.length
+		end
+	end
+
 end
 
 begin
@@ -43,6 +65,8 @@ begin
     Todo.new(true, "app ruby"),
   ]
   curr_todo = 0
+  curr_done = 0
+  tab = "TODO"
   height = Curses.lines
   width = Curses.cols
   
@@ -64,36 +88,26 @@ begin
     begin_loop = 1
     ui.label(0, (width / 4) - 8 , "TODO [ ]", REGULAR_PAIR, todowin)
     todos.each_with_index do |todo, index|
-      if index == curr_todo
+      if index == curr_todo and tab == 'TODO'
         pair = HIGHLIGHT_PAIR
       else
         pair = REGULAR_PAIR
       end
       #puts todo.completed
-      if (todo.completed == false)
         text = "[ ] #{todo.text}"
         ui.label(index + begin_loop, 0 + 1, text,pair, todowin)
-      else
-        text = "[x] #{todo.text}"
-        ui.label(index + begin_loop, 0 + 1, text,pair, donewin)
-      end
     end
 
     ui.label(0, (width / 4) - 8 , "DONE [x]", REGULAR_PAIR, donewin)
-    dones.each_with_index do |todo, index|
-      if index == curr_todo
+    dones.each_with_index do |done, index|
+      if index == curr_done and tab == 'DONE'
         pair = HIGHLIGHT_PAIR
       else
         pair = REGULAR_PAIR
       end
       #puts todo.completed
-      if (todo.completed == false)
-        text = "[ ] #{todo.text}"
-        ui.label(index + begin_loop, 0 + 1, text,pair, todowin)
-      else
-        text = "[x] #{todo.text}"
+        text = "[x] #{done.text}"
         ui.label(index + begin_loop , 0 + 1, text,pair, donewin)
-      end
     end
     
     bottombar = "`q` to quit."
@@ -113,11 +127,37 @@ begin
     when 'q'
       quit = true
     when 'A'
-      curr_todo = curr_todo - 1 if curr_todo > 0
+      case tab
+			when 'TODO'
+				curr_todo = ui.list_up(todos, curr_todo)
+			when 'DONE'
+				curr_done = ui.list_up(dones, curr_done)
+			end
     when 'B'
-      curr_todo = curr_todo + 1 if curr_todo < todos.length - 1 
+      case tab
+			when 'TODO'
+				curr_todo = ui.list_down(todos, curr_todo)
+			when 'DONE'
+				curr_done = ui.list_down(dones, curr_done)
+			end
+    when 9
+			tab = ui.toggle(tab)
     when 10
-      todos[curr_todo].completed = !todos[curr_todo].completed
+      case tab
+      when 'TODO'
+        if todos.length > 0
+        todos[curr_todo].completed = !todos[curr_todo].completed
+        dones.push(todos[curr_todo])
+        todos.delete(todos[curr_todo])
+        end
+      when 'DONE'
+        if dones.length > 0
+        dones[curr_done].completed = !dones[curr_done].completed
+        todos.push(dones[curr_done])
+        dones.delete(dones[curr_done])
+        end
+      end
+     
     end
 
   end
